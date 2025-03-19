3f3d8893432ba4d83c824c7ba179b4f2ce7eaf1c
hugo <hugopingusson@icloud.com>
2025-03-19 23:26:57 +0000

    dev

--- Core/Node/class_Node.cpp
+++ Core/Node/class_Node.cpp
@@ -4,6 +4,8 @@
 
 #include "class_Node.h"
 
+#include <math.h>
+
 Node::Node() {
     sequence_number = int64_t();
     last_exchange_timestamp=int64_t();
@@ -18,4 +20,73 @@ Node::Node(const string &name) {
     last_reception_timestamp=int64_t();
     valid=false;
     this->name=name;
+}
+
+string Node::get_name() const {
+    return this->name;
+}
+
+bool Node::is_valid() const {
+    return this->valid;
+}
+
+int64_t Node::get_last_exchange_timestamp() const {
+    return this->last_exchange_timestamp;
+}
+
+int64_t Node::get_last_reception_timestamp() const {
+    return this->last_reception_timestamp;
+}
+
+
+
+ValidCollector::ValidCollector():valid_map(map<const Node*, bool>()) {}
+ValidCollector::ValidCollector(const map<const Node *, bool>& valid_map):valid_map(valid_map) {}
+
+
+void ValidCollector::update(const Node& node) {
+    this->valid_map.at(&node)=node.is_valid();
+}
+
+
+ValueNode::ValueNode():Node(),last_value(std::nan("")){}
+ValueNode::ValueNode(const string &name):Node(name),last_value(std::nan("")) {}
+
+
+double ValueNode::get_last_value() const {
+    return this->last_value;
+}
+
+
+MonoValueNode::MonoValueNode():ValueNode() {}
+MonoValueNode::MonoValueNode(const string &name):ValueNode(name) {}
+
+void MonoValueNode::update(const Node &node) {
+    if (node.is_valid()) {
+        this->last_value=this->compute(node);
+        if (std::isnan(this->last_value)) {
+            this->valid=false;
+        }
+        else {
+            this->valid=true;
+        }
+    }else {
+        this->valid=false;
+    }
+}
+
+
+
+MultiValueNode::MultiValueNode():ValueNode(){
+    valid_collector = ValidCollector();
+
+}
+
+MultiValueNode::MultiValueNode(const string &name):ValueNode(name) {
+    valid_collector = ValidCollector();
+}
+
+void MultiValueNode::update(const Node& node) {
+    this->valid_collector.update(node);
+
 }
--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 
+#include "Core/Node/class_Node.h"
 #include "DataReader/class_DataReader.h"
 #include "Helper/class_FutureHelper.h"
 #include "Logger/class_Logger.h"
-
+#include "Core/Node/class_Node.h"
 
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -12,11 +13,17 @@ int main() {
     // DataReader data_reader = DataReader();
     // auto test = data_reader.get_cme_market_data_table("EURUSD","2024-04-29");
 
-    Logger logger = Logger();
-    logger.log_error("Ceci est une error");
-    logger.log_info("Ceci est une info");
+    double test = 1;
+
+    std::to_string(&test);
+
+
+
+    // Logger logger = Logger();
+    // logger.log_error("Ceci est une error");
+    // logger.log_info("Ceci est une info");
     // auto file_logger = spdlog::basic_logger_mt("file_logger", "logs.txt");
-    // return 1;
+    return 1;
 }
 
 // TIP See CLion help at <a
